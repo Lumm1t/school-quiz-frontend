f9c1d7f8e15c72624e8e6787adb0cf7fa36599e1
Lumm1t <aleksanderszolowicz@gmail.com>
2021-05-02 20:41:26 +0200

    feat: add question template

---
+++ components/organisms/QuizQuestion.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card width="700" class="mx-auto mt-5">
+    <v-card-title class="pb-0">
+      <h3>1/1 - pytanie</h3>
+    </v-card-title>
+
+    <v-divider></v-divider>
+
+    <v-card-text>
+      <p>{{ radios || 'null' }}</p>
+
+      <v-radio-group v-model="radios" mandatory>
+        <v-radio label="Radio 1" value="radio-1"></v-radio>
+        <v-radio label="Radio 2" value="radio-2"></v-radio>
+        <v-radio label="Radio 3" value="radio-3"></v-radio>
+        <v-radio label="Radio 4" value="radio-4"></v-radio>
+      </v-radio-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'QuizQuestion',
+  data() {
+    return {
+      radios: [],
+    }
+  },
+}
+</script>
---
+++ pages/quiz.vue
@@ -0,0 +1,3 @@
+<template>
+  <QuizQuestion />
+</template>
