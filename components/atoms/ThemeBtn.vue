f2400c68ce2fdc2cd2ff2f6f6fd821d5615d66e3
Lumm1t <aleksanderszolowicz@gmail.com>
2021-05-03 15:05:31 +0200

    feat: persist dark theme

--- components/atoms/ThemeBtn.vue
+++ components/atoms/ThemeBtn.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-btn icon @click="switchTheme()">
-    <v-icon>mdi-brightness-6</v-icon>
-  </v-btn>
+  <v-tooltip bottom>
+    <template #activator="{ on }">
+      <v-btn icon @click="changeTheme" v-on="on">
+        <v-icon>mdi-brightness-6</v-icon>
+      </v-btn>
+    </template>
+
+    <span>Zmień Tło</span>
+  </v-tooltip>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+import setupTheme from '@/assets/setupTheme'
+
 export default {
   name: 'ThemeBtn',
-  methods: {
-    switchTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
-    },
+  mounted() {
+    setupTheme.call(this)
   },
+  methods: mapMutations(['changeTheme']),
 }
 </script>
