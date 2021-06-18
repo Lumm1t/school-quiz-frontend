cae2fe064008099498e654a84dd37dcd21819fcf
Lumm1t <aleksanderszolowicz@gmail.com>
2021-06-18 13:18:55 +0200

    feat(basic settings): add form

--- components/organisms/admin/exam/BasicSettings.vue
+++ components/organisms/admin/exam/BasicSettings.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>basic settings</div>
+  <v-card width="500">
+    <v-card-title>
+      <h1>Ustawienia wstępne</h1>
+    </v-card-title>
+
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="examName"
+          label="Nazwa testu"
+          prepend-icon="mdi-account-circle"
+          required
+        />
+
+        <v-text-field
+          v-model="examDescription"
+          label="Opis testu"
+          prepend-icon="mdi-account-circle"
+          required
+        />
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions class="d-flex justify-end">
+      <v-btn color="primary" @click="saveBasicSettings">Zapisz</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -7,5 +33,14 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'BasicSettings',
+  data: () => ({
+    examName: '',
+    examDescription: '',
+  }),
+  methods: {
+    saveBasicSettings() {
+      console.log(this.examName, this.examDescription)
+    },
+  },
 })
 </script>
