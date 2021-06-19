32bb606295a072e99cee6a86d40a606fe1323ce7
Lumm1t <aleksanderszolowicz@gmail.com>
2021-06-19 17:34:44 +0200

    feat(time parameters): add design

--- components/organisms/admin/exam/TimeParameters.vue
+++ components/organisms/admin/exam/TimeParameters.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>time parameters</div>
+  <v-card>
+    <v-card-title>Parametry czasowe</v-card-title>
+
+    <v-card-text>
+      Limit czasowy na każde pytanie występujące w teście (podany w sekundach):
+
+      <v-text-field
+        v-model="questionAnswerTime"
+        style="width: 100px"
+        type="number"
+      ></v-text-field>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="primary" @click="saveQuestionAnswerTime">Zapisz</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -7,5 +23,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'TimeParameters',
+  data: () => ({
+    questionAnswerTime: 60,
+  }),
+  methods: {
+    saveQuestionAnswerTime() {
+      console.log(this.questionAnswerTime)
+    },
+  },
 })
 </script>
