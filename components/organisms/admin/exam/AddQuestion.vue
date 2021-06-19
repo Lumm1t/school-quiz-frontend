143a4368eea5ccfe289a83403397f2461f4e5f35
Lumm1t <aleksanderszolowicz@gmail.com>
2021-06-19 17:12:57 +0200

    feat(add question): change logic

--- components/organisms/admin/exam/AddQuestion.vue
+++ components/organisms/admin/exam/AddQuestion.vue
@@ -5,93 +5,70 @@
     </v-card-title>
 
     <v-card-text>
+      <!-- Question title -->
       <h2>Treść pytania:</h2>
+
       <v-col class="d-flex" cols="12" sm="8">
-        <TextEditor v-model="newQuestion.title" class="my-5" />
+        <TextEditor v-model="questionTitle" class="my-5" />
       </v-col>
 
+      <!-- Answer type -->
       <h2>Typ odpowiedzi:</h2>
       <v-col class="d-flex" cols="12" sm="6">
         <v-select
-          v-model="newQuestion.type"
+          v-model="questionType"
           :items="questionTypes"
           filled
           single-line
-          @change="resetCorrectAnswer"
         ></v-select>
       </v-col>
 
-      <h2>Odpowiedzi:</h2>
-      <v-btn color="primary" text @click="newQuestion.answers.push('')">
+      <!-- Question answers -->
+      <v-btn color="primary" text @click="++numberOfAnswers">
         Dodaj odpowiedź
       </v-btn>
 
-      <!-- single & multi -->
-      <div
-        v-if="
-          newQuestion.type === 'Jednokrotny wybór' ||
-          newQuestion.type === 'Wielokrotny wybór'
-        "
-      >
-        <v-col class="d-flex flex-column" cols="12" sm="8">
-          <div v-for="i in answersLength" :key="i">
+      <v-btn color="primary" text @click="--numberOfAnswers">
+        Usuń odpowiedź
+      </v-btn>
+
+      <v-col class="d-flex flex-column" cols="12" sm="8">
+        <div v-for="i in numberOfAnswers" :key="i">
+          <div v-if="questionType === 'single' || questionType === 'multi'">
             <h4>Odpowiedź {{ i }}</h4>
-            <v-btn
-              color="error"
-              small
-              @click="newQuestion.answers.splice(i - 1, 1)"
-            >
-              Usuń
-            </v-btn>
 
-            <TextEditor v-model="newQuestion.answers[i - 1]" class="my-5" />
+            <TextEditor v-model="questionAnswers[i - 1]" class="my-5" />
           </div>
-        </v-col>
 
-        <div v-if="newQuestion.type === 'Jednokrotny wybór'">
-          <h2>Poprawna odpowiedź:</h2>
-          <v-col cols="2">
-            <v-select
-              v-model="newQuestion.correctAnswer"
-              :items="answersToSelect"
-              single-line
-            ></v-select>
-          </v-col>
-        </div>
+          <div v-else-if="questionType === 'text'">
+            <h4>Odpowiedź {{ i }}</h4>
 
-        <div v-else-if="newQuestion.type === 'Wielokrotny wybór'">
-          <h2>Poprawna odpowiedź:</h2>
-          <v-col cols="2">
-            <v-select
-              v-model="newQuestion.correctAnswer"
-              :items="answersToSelect"
-              multiple
-              single-line
-            ></v-select>
-          </v-col>
-        </div>
-      </div>
-
-      <!-- text -->
-      <v-col
-        v-if="newQuestion.type === 'Krótka odpowiedź'"
-        class="d-flex flex-column"
-        cols="12"
-        sm="8"
-      >
-        <div v-for="i in answersLength" :key="i">
-          <h4>Odpowiedź {{ i }}</h4>
-          <v-btn
-            color="error"
-            small
-            @click="newQuestion.answers.splice(i - 1, 1)"
-          >
-            Usuń
-          </v-btn>
-
-          <TextEditor v-model="newQuestion.correctAnswer[i - 1]" class="my-5" />
+            <TextEditor v-model="questionCorrectAnswers[i - 1]" class="my-5" />
+          </div>
         </div>
       </v-col>
+
+      <div v-if="questionType === 'single'">
+        <h2>Poprawna odpowiedź:</h2>
+        <v-col cols="2">
+          <v-select
+            v-model="questionCorrectAnswers"
+            :items="answersToSelect"
+            single-line
+          ></v-select>
+        </v-col>
+      </div>
+      <div v-else-if="questionType === 'multi'">
+        <h2>Poprawne odpowiedzi:</h2>
+        <v-col cols="2">
+          <v-select
+            v-model="questionCorrectAnswers"
+            :items="answersToSelect"
+            multiple
+            single-line
+          ></v-select>
+        </v-col>
+      </div>
     </v-card-text>
 
     <v-card-actions>
@@ -103,39 +80,47 @@
 <script lang="ts">
 import Vue from 'vue'
 
+interface question {
+  title: string
+  type: string
+  answers?: any[]
+  correctAnswers: any[] | number | string
+}
+
 export default Vue.extend({
   name: 'AddQuestion',
   data: () => ({
-    newQuestion: {
-      title: '',
-      type: 'Jednokrotny wybór',
-      answers: ['', '', ''],
-      correctAnswer: 0 as number | number[],
-    },
-    questionTypes: [
-      'Jednokrotny wybór',
-      'Wielokrotny wybór',
-      'Krótka odpowiedź',
-    ],
+    questionTitle: '',
+    questionType: 'single',
+    questionAnswers: [],
+    questionCorrectAnswers: [],
+    questionTypes: ['single', 'multi', 'text'],
+    numberOfAnswers: 2,
   }),
   computed: {
-    answersLength(): number {
-      return this.newQuestion.answers.length
-    },
     answersToSelect(): number[] {
-      // array from 1 to n
-      return [...Array.from({ length: this.answersLength }).keys()].map(
-        (i) => i
-      )
+      // array from 0 to n
+      return [...Array.from({ length: this.numberOfAnswers }).keys()]
+    },
+  },
+  watch: {
+    questionType() {
+      this.questionAnswers = []
+      this.questionCorrectAnswers = []
     },
   },
   methods: {
-    resetCorrectAnswer() {
-      this.newQuestion.answers = ['', '', '']
-      this.newQuestion.correctAnswer = []
-    },
     addNewQuestion() {
-      console.log(this.newQuestion)
+      const newQuestion: question = {
+        title: this.questionTitle,
+        type: this.questionType,
+        correctAnswers: this.questionCorrectAnswers,
+      }
+
+      if (this.questionType !== 'text')
+        newQuestion.answers = this.questionAnswers
+
+      console.log(newQuestion)
     },
   },
 })
